b0ea62c9e983927143500ed5455203fd61b9e42f
FroopleXP <connor@frooplexp.com>
2017-03-31 17:30:55 +0100

    Added more things...

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -2,4 +2,31 @@
  * Created by conno on 31/03/2017.
  */
 public class BreadthFirstSearch {
+
+    // Creating a mock network, this will later be generated via JSON
+    private breadthNode nodeOne = new breadthNode(1);
+    private breadthNode nodeTwo = new breadthNode(2);
+    private breadthNode nodeThree = new breadthNode(3);
+    private breadthNode nodeFour = new breadthNode(4);
+    private breadthNode nodeFive = new breadthNode(5);
+
+    // Creating the adjacent nodes for each node
+    private breadthNode[] nodeOneAdj = new breadthNode[] { nodeTwo, nodeFive };
+    private breadthNode[] nodeTwoAdj = new breadthNode[] { nodeOne, nodeThree, nodeFour };
+    private breadthNode[] nodeThreeAdj = new breadthNode[] { nodeTwo, nodeFour, nodeFive };
+    private breadthNode[] nodeFourAdj = new breadthNode[] { nodeTwo, nodeThree };
+    private breadthNode[] nodeFiveAdj = new breadthNode[] { nodeThree, nodeOne };
+
+    // Adding the adjacent nodes to the nodes
+
+    // Adding the nodes to the network
+    breadthNode[] network = new breadthNode[] {
+            nodeOne, nodeTwo, nodeThree, nodeFour, nodeFive
+    };
+
+    public static void main(String[] args) {
+        System.out.print("This is a test...");
+    }
+
+
 }
---
+++ src/breadthNode.java
@@ -0,0 +1,33 @@
+/**
+ * Created by conno on 31/03/2017.
+ */
+public class breadthNode {
+
+    private int nodeId; // Holds the ID of the Node
+
+    private breadthNode[] adjNodes; // Holds the adjacent nodes
+    private boolean searched; // Holds the value of whether or not this node has been searched
+    private breadthNode parent; // Will contain the ID of it's parent
+
+    // When a new Node is created it is given an ID
+    breadthNode(int nodeId) {
+        this.nodeId = nodeId;
+        this.searched = false;
+    }
+
+    // Used to set the adjacent node of the node
+    public void setAdjNodes(breadthNode[] nodes) {
+        this.adjNodes = nodes;
+    }
+
+    // Used to set the searched value (True/false)
+    public void setSearched(boolean searched) {
+        this.searched = searched;
+    }
+
+    // Used to set the parent node
+    public void setParent(breadthNode parentNode) {
+        this.parent = parentNode;
+    }
+
+}
